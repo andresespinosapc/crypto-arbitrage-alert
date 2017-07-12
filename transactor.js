899dcb92da73e3976080086e5fdf80d54393a646
sebamenabar <samenabar@uc.cl>
2017-07-12 19:28:53 -0400

    transactor improvements

--- transactor.js
+++ transactor.js
@@ -1,36 +1,38 @@
 const Tx = require('ethereumjs-tx')
 const Web3 = require('web3')
+const BigNumber = require('bignumber.js');
 const ethUtil = require('ethereumjs-util')
-const defaultGasPrice = '0x77359400' // 2 gWei
-const defaultGasLimit = '0x286a0' // 100.000
+const defaultGasPrice = 2 // 2 gWei
+const defaultGasLimit = 100000 // 100.000
 
 function TS() {}
 
-TS.init = function(provider)
+TS.init = function({provider, privateKey}={})
 {
   this.web3 = new Web3(new Web3.providers.HttpProvider(provider))
+  this.privateKey = privateKey
+  this.address = '0x' + ethUtil.privateToAddress(privateKey).toString('hex')
 }
 
-TS.transact = function(privateKey, to, value=0, nonce=null, gasPrice=defaultGasPrice, data='', gasLimit=defaultGasLimit)
+TS.transact = function({to, value=0, gasPrice=defaultGasPrice, gasLimit=defaultGasLimit, data='', nonce=null}={})
 {
   if(nonce===null){
-    var from = ethUtil.privateToAddress(privateKey);
-    var nonce = '0x' + this.web3.eth.getTransactionCount('0x' + from.toString('hex')).toString(10)
+    var nonce = this.web3.eth.getTransactionCount(this.address)
   }
   var params = {
     nonce:nonce,
-    gasPrice:gasPrice,
+    gasPrice: '0x' + new BigNumber(this.web3.toWei(gasPrice, 'gwei')).toString(16),
     gasLimit:gasLimit,
     to:to,
-    value:value,
+    value: '0x' + new BigNumber(this.web3.toWei(value, 'ether')).toString(16),
     data:data
   }
-
-  var tx = new Tx(params)
-  tx.sign(priv)
+  var tx = new Tx(params);
+  tx.sign(this.privateKey)
+  return tx
   var serializedTx = tx.serialize()
+  return serializedTx;
   txHash = this.web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'))
-  return txHash
 }
 
 module.exports = TS
