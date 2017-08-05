fd26d3248715dd55168733ad71befb156983b9a1
Andres Espinosa <andresespinosapc@gmail.com>
2017-08-04 23:37:17 -0400

    Add amenabar's user. Little fix

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const AlertDiff = require('./alert_diff.js');
 const mysql = require('mysql');
 const TS = require('./transactor.js');
 const websites = require('./websites/exports.js');
+const fs = require('fs');
 
 
 let privateKey = Buffer.from(process.env.PRIV, 'hex');
@@ -13,17 +14,15 @@ let myWebsites = {
   liqui: new websites.Liqui(TS, process.env.LIQUI_KEY, process.env.LIQUI_SECRET)
 }
 
-const TELEGRAM_CHAT_ID = 8834684;
 const TELEGRAM_BOT_TOKEN = '423299318:AAGSZaf9hy8_KNy2QAtLebSA_9uJovuc4sU';
-
-let userSettings = {
-  chatId: TELEGRAM_CHAT_ID,
-  coins: ['PAY', 'EOS', 'SNT', 'FUN', 'ADX', 'BAT', 'OMG', 'NET', 'BNT'],
-  blacklist: ['OMG', 'BAT'],
-  arbitrageAlert: true,
-  etherdeltaDiffAlert: true,
-  etherdeltaCheapAlert: true
-};
+let telegramChatIds = [
+  '8834684',
+  '139887252'
+]
+let userSettings = {};
+telegramChatIds.forEach((chatId) => {
+  userSettings[chatId] = JSON.parse(fs.readFileSync(chatId + '.json', 'utf8'));
+});
 
 let pool;
 if (process.env.IS_DG) {
@@ -48,66 +47,66 @@ else {
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
 
 bot.onText(/move (.+) (.+) from (.+) to (.+)/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     let amount = parseFloat(match[1]);
     let token = match[2];
     let initialWebsite = match[3];
     let finalWebsite = match[4];
 
     TS.moveCoin(myWebsites[initialWebsite], myWebsites[finalWebsite], token, amount, (err) => {
-      if (err) bot.sendMessage(userSettings.chatId, err);
+      if (err) bot.sendMessage(msg.chat.id, err);
       else {
-        bot.sendMessage(userSettings.chatId, 'Your coin movement has finished');
+        bot.sendMessage(msg.chat.id, 'Your coin movement has finished');
       }
     });
   }
 });
 
-bot.onText(/withdraw (.+) (.+) from (.+)/, (msg, macth) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+bot.onText(/withdraw (.+) (.+) from (.+)/, (msg, match) => {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     let amount = parseFloat(match[1]);
     let token = match[2];
     let website = match[3];
 
     myWebsites[website].withdraw(token, amount, (err) => {
-      if (err) bot.sendMessage(userSettings.chatId, err);
+      if (err) bot.sendMessage(msg.chat.id, err);
       else {
-        bot.sendMessage(userSettings.chatId, 'Withdrawal successful');
+        bot.sendMessage(msg.chat.id, 'Withdrawal successful');
       }
     });
   }
 });
 
-bot.onText(/deposit (.+) (.+) on (.+)/, (msg, macth) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+bot.onText(/deposit (.+) (.+) on (.+)/, (msg, match) => {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     let amount = parseFloat(match[1]);
     let token = match[2];
     let website = match[3];
 
     myWebsites[website].deposit(token, amount, (err) => {
-      if (err) bot.sendMessage(userSettings.chatId, err);
+      if (err) bot.sendMessage(msg.chat.id, err);
       else {
-        bot.sendMessage(userSettings.chatId, 'Deposit successful');
+        bot.sendMessage(msg.chat.id, 'Deposit successful');
       }
     });
   }
 });
 
 bot.onText(/order (buy|sell) (.+) (.+) at (.+)/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     let direction = match[1];
     let amount = parseFloat(match[2]);
     let token = match[3];
     let price = parseFloat(match[4]);
 
     myWebsites.etherdelta.order(direction, amount, price, 'ETH', token, 10000, false, (err, orderNonce) => {
-      if (err) bot.sendMessage(userSettings.chatId, err);
+      if (err) bot.sendMessage(msg.chat.id, err);
       else {
         console.log('Order placed with nonce: ' + orderNonce);
         myWebsites.etherdelta.waitForOrder(direction, 'ETH', token, orderNonce, (err) => {
-          if (err) bot.sendMessage(userSettings.chatId, err);
+          if (err) bot.sendMessage(msg.chat.id, err);
           else {
-            bot.sendMessage(userSettings.chatId, 'Termino tu orden');
+            bot.sendMessage(msg.chat.id, 'Termino tu orden');
           }
         });
       }
@@ -116,80 +115,89 @@ bot.onText(/order (buy|sell) (.+) (.+) at (.+)/, (msg, match) => {
 });
 
 bot.onText(/toggle ethdelta cheap/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
-    userSettings.etherdeltaCheapAlert = !userSettings.etherdeltaCheapAlert;
-    let currentState = (userSettings.etherdeltaCheapAlert) ? 'ON' : 'OFF';
-    bot.sendMessage(userSettings.chatId, 'Ok, now it is ' + currentState);
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    userSettings[msg.chat.id].etherdeltaCheapAlert = !userSettings[msg.chat.id].etherdeltaCheapAlert;
+    let currentState = (userSettings[msg.chat.id].etherdeltaCheapAlert) ? 'ON' : 'OFF';
+    bot.sendMessage(msg.chat.id, 'Ok, now it is ' + currentState);
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/toggle ethdelta diff/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
-    userSettings.etherdeltaDiffAlert = !userSettings.etherdeltaDiffAlert;
-    let currentState = (userSettings.etherdeltaDiffAlert) ? 'ON' : 'OFF';
-    bot.sendMessage(userSettings.chatId, 'Ok, now it is ' + currentState);
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    userSettings[msg.chat.id].etherdeltaDiffAlert = !userSettings[msg.chat.id].etherdeltaDiffAlert;
+    let currentState = (userSettings[msg.chat.id].etherdeltaDiffAlert) ? 'ON' : 'OFF';
+    bot.sendMessage(msg.chat.id, 'Ok, now it is ' + currentState);
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/toggle arbitrage/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
-    userSettings.arbitrageAlert = !userSettings.arbitrageAlert;
-    let currentState = (userSettings.arbitrageAlert) ? 'ON' : 'OFF';
-    bot.sendMessage(userSettings.chatId, 'Ok, now it is ' + currentState);
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    userSettings[msg.chat.id].arbitrageAlert = !userSettings[msg.chat.id].arbitrageAlert;
+    let currentState = (userSettings[msg.chat.id].arbitrageAlert) ? 'ON' : 'OFF';
+    bot.sendMessage(msg.chat.id, 'Ok, now it is ' + currentState);
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/add coin (.+)/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     const arg = match[1];
-    userSettings.coins.push(arg)
-    bot.sendMessage(userSettings.chatId, 'Coin added. ' + JSON.stringify(userSettings.coins));
+    userSettings[msg.chat.id].coins.push(arg)
+    bot.sendMessage(msg.chat.id, 'Coin added. ' + JSON.stringify(userSettings[msg.chat.id].coins));
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/remove coin (.+)/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     const arg = match[1];
-    let index = userSettings.coins.indexOf(arg);
+    let index = userSettings[msg.chat.id].coins.indexOf(arg);
     if (index > -1) {
-      userSettings.blacklist.splice(index, 1);
-      bot.sendMessage(userSettings.chatId, 'Coin removed. ' + JSON.stringify(userSettings.coins));
+      userSettings[msg.chat.id].blacklist.splice(index, 1);
+      bot.sendMessage(msg.chat.id, 'Coin removed. ' + JSON.stringify(userSettings[msg.chat.id].coins));
     }
     else {
-      bot.sendMessage(userSettings.chatId, 'Mmm not in the list');
+      bot.sendMessage(msg.chat.id, 'Mmm not in the list');
     }
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/remove blacklist (.+)/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
     const arg = match[1];
-    let index = userSettings.blacklist.indexOf(arg);
+    let index = userSettings[msg.chat.id].blacklist.indexOf(arg);
     if (index > -1) {
-      userSettings.blacklist.splice(index, 1);
-      bot.sendMessage(userSettings.chatId, 'Ok, removed. ' + JSON.stringify(userSettings.blacklist));
+      userSettings[msg.chat.id].blacklist.splice(index, 1);
+      bot.sendMessage(msg.chat.id, 'Ok, removed. ' + JSON.stringify(userSettings[msg.chat.id].blacklist));
     }
     else {
-      bot.sendMessage(userSettings.chatId, 'Mmm not in the list');
+      bot.sendMessage(msg.chat.id, 'Mmm not in the list');
     }
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
   }
 });
 
 bot.onText(/blacklist (.+)/, (msg, match) => {
-  const arg = match[1];
-  userSettings.blacklist.push(arg);
-  bot.sendMessage(userSettings.chatId, 'Ok, added. ' + JSON.stringify(userSettings.blacklist));
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    const arg = match[1];
+    userSettings[msg.chat.id].blacklist.push(arg);
+    bot.sendMessage(msg.chat.id, 'Ok, added. ' + JSON.stringify(userSettings[msg.chat.id].blacklist));
+    fs.writeFile(msg.chat.id + '.json', JSON.stringify(userSettings[msg.chat.id]));
+  }
 });
 
 bot.onText(/show coins/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
-    bot.sendMessage(userSettings.chatId, JSON.stringify(userSettings.coins));
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    bot.sendMessage(msg.chat.id, JSON.stringify(userSettings[msg.chat.id].coins));
   }
 });
 
 bot.onText(/show blacklist/, (msg, match) => {
-  if (msg.chat.id == TELEGRAM_CHAT_ID) {
-    bot.sendMessage(userSettings.chatId, JSON.stringify(userSettings.blacklist));
+  if (telegramChatIds.indexOf(msg.chat.id) != -1) {
+    bot.sendMessage(msg.chat.id, JSON.stringify(userSettings[msg.chat.id].blacklist));
   }
 });
 
@@ -199,4 +207,4 @@ let allCoins = [
   'FUCK', 'ADX', 'BAT', 'OMG'
 ];
 
-AlertDiff.start(pool, bot, userSettings, myWebsites);
+AlertDiff.start(pool, bot, userSettings[msg.chat.id], myWebsites);
